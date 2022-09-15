aac33dc208c889837e9d1bbb7c1c252ae217de6b
Kevin edoho henry <henrykevin118@gmail.com>
2022-09-15 13:30:26 -0700

    task i

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h
+
+/**
+ * main - test function that prints if integer is postive or negative
+ * Return: 0
+ */
+
+int main(void) 
+{
+	int 1;
+
+	i = 0;
+	postive_or_negative(i);
+
+	return (0); 
+} 
+
